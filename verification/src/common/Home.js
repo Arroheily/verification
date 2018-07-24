5290a4a5c24961cb20d83176b9859b68945660c2
寸晖 吴 <cunhui.wu@sail-fs.com>
2018-07-24 18:01:18 +0800

    update

---
+++ verification/src/common/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <menu>
+                    <div>这是第1行菜单</div>
+                    <div>这是第1行菜单</div>
+                    <div>这是第1行菜单</div>
+                    <div>这是第1行菜单</div>
+                </menu>
+            </div>
+        );
+    }
+}
+
+export default Home;
